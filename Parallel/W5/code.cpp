95d96b0ea6a9f5297a7773c4a271aef8bf51d6a6
jeffreymonjacastro <jeffrey.monja@utec.edu.pe>
2025-09-23 17:28:48 -0500

    Adding mpich compiler for khipu

--- Parallel/W5/code.cpp
+++ Parallel/W5/code.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-  int rank, size, numero = 0, count;
+  int rank, size;
 
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
