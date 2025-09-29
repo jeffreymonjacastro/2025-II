8d5b8b8d1edf3678994f75b0af91b42db51594f8
jeffreymonjacastro <jeffrey.monja@utec.edu.pe>
2025-09-29 18:19:40 -0500

    Week 7 Parallel

--- Parallel/W7/teo/ejemplo02_send_row.cpp
+++ Parallel/W7/teo/ejemplo02_send_row.cpp
@@ -9,30 +9,32 @@
 #define N 6
 #define M 6
 #define f 1
-int main(int argc, char* argv[]) {
-    int i, j;
-    int p;
-    int my_rank;
-    float A[N][M]={0};
-    MPI_Status status;
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+int main(int argc, char *argv[]) {
+  int i, j;
+  int p;
+  int my_rank;
+  float A[N][M] = {0};
 
-    if (my_rank == 0) {
-        for (i = 0; i < N; i++)
-            for (j = 0; j < M; j++)
-                A[i][j] = (float) i;
-// 1. Rank 0 envíauna fila de matriz A a proceso 1
-      MPI_Send(A[f],M,MPI_FLOAT,1,0,MPI_COMM_WORLD); 
-    } else { /* my_rank = 1 */
-// 2. Rank 1 recibe e imprime su matriz con la fila enviada
-      MPI_Recv(A[f],M,MPI_FLOAT,0,0,MPI_COMM_WORLD,&status); 
-        for (i = 0; i < N; i++){
-		for (j = 0; j < M; j++)
-            		printf("%3.1f ", A[i][j]);
-        printf("\n");
-    }}
+  MPI_Status status;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-    MPI_Finalize();
-} 
+  if (my_rank == 0) {
+    for (i = 0; i < N; i++)
+      for (j = 0; j < M; j++)
+        A[i][j] = (float)i;
+    // 1. Rank 0 envíauna fila de matriz A a proceso 1
+    MPI_Send(A[f], M, MPI_FLOAT, 1, 0, MPI_COMM_WORLD);
+  } else { /* my_rank = 1 */
+    // 2. Rank 1 recibe e imprime su matriz con la fila enviada
+    MPI_Recv(A[f], M, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, &status);
+    for (i = 0; i < N; i++) {
+      for (j = 0; j < M; j++)
+        printf("%3.1f ", A[i][j]);
+      printf("\n");
+    }
+  }
+
+  MPI_Finalize();
+}
--- Parallel/W7/teo/ejemplo03_matrices.cpp
+++ Parallel/W7/teo/ejemplo03_matrices.cpp
@@ -1,63 +1,66 @@
-# include <iostream>
-# include <mpi.h>
+#include <iostream>
+#include <mpi.h>
 #define N 8
 #define M 4
-void imprimir_matriz(double A[][M], double B[][M]){
-printf("Matriz A\n");
-for (int i=0; i<N;i++) {
-for (int j=0; j<M;j++) {
-        printf("%f ",A[i][j]);
-}
-printf("\n");
-}
-printf("Matriz B\n");
-for (int i=0; i<N;i++) {
-for (int j=0; j<M;j++) {
-        printf("%f ",B[i][j]);
-}
-printf("\n");
-}
+
+void imprimir_matriz(double A[][M], double B[][M]) {
+  printf("Matriz A\n");
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < M; j++) {
+      printf("%f ", A[i][j]);
+    }
+    printf("\n");
+  }
+  printf("Matriz B\n");
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < M; j++) {
+      printf("%f ", B[i][j]);
+    }
+    printf("\n");
+  }
 }
 
-int main ( int argc, char *argv[] )
-{
-int p, rank, i, j, mb;
-        double A[N][M], B[N][M], Al[N][M], Bl[N][M];
-        MPI_Init(&argc, &argv);
- 	MPI_Comm_size(MPI_COMM_WORLD, &p);
-     	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+int main(int argc, char *argv[]) {
+  int p, rank, i, j, mb;
+  double A[N][M], B[N][M], Al[N][M], Bl[N][M];
 
-if (rank==0) {
-for (int i=0; i<N;i++) {
-for (int j=0; j<M;j++) {
-        A[i][j]=j;
-        B[i][j]=2*j;
-}
-}
-printf("Matrices origen\n");
-imprimir_matriz(A,B);
-}
-// 1. Envie N/p filas a cada proceso, donde se almacenan 
-//    submatrices Al, Bl de A, B
-mb = N/p;
-//     for (i=0;i<mb;i++) {
-       MPI_Scatter();
-	
-//}
- for (i=0;i<mb;i++)
- 	for (j=0;j<M;j++){
-	  	Al[i][j] *= 2;
-	  	Bl[i][j] *= 2;
-}
-// 2. Envie las submatrices desde cada proceso al maestro
- for (i=0;i<mb;i++){
-//       MPI_Gather . . .
-}
-if (rank==0) {
-printf("Matrices destino\n");
-imprimir_matriz(A,B);
-}
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &p);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-MPI_Finalize();
+  if (rank == 0) {
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < M; j++) {
+        A[i][j] = j;
+        B[i][j] = 2 * j;
+      }
+    }
+    printf("Matrices origen\n");
+    imprimir_matriz(A, B);
+  }
 
+  // 1. Envie N/p filas a cada proceso, donde se almacenan
+  //    submatrices Al, Bl de A, B
+  mb = N / p;
+  for (i = 0; i < mb; i++) {
+    MPI_Scatter(A, M, MPI_DOUBLE, Al[i], M, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+  }
+
+  for (i = 0; i < mb; i++) {
+    for (j = 0; j < M; j++) {
+      Al[i][j] *= 2;
+      Bl[i][j] *= 2;
+    }
+  }
+
+  // 2. Envie las submatrices desde cada proceso al maestro
+  for (i = 0; i < mb; i++) {
+    MPI_Gather(Al[i], M, MPI_DOUBLE, A[i * p], M, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+  }
+  if (rank == 0) {
+    printf("Matrices destino\n");
+    imprimir_matriz(A, B);
+  }
+
+  MPI_Finalize();
 }
