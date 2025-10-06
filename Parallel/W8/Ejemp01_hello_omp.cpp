c53113ef91f577eafe6adfd37296b686f8022d3c
jeffreymonjacastro <jeffrey.monja@utec.edu.pe>
2025-10-06 18:02:02 -0500

    Parallel

--- Parallel/W8/Ejemp01_hello_omp.cpp
+++ Parallel/W8/Ejemp01_hello_omp.cpp
@@ -1,32 +1,31 @@
 #include <iostream>
 #include <unistd.h>
+
 #ifdef _OPENMP
-#include <omp.h>
+  #include <omp.h>
 #endif
+
 using namespace std;
 
 int main() {
-int id=-1;
-// 1. Genere una recion en paralelo 
-// con variable compartida id
-// 3. Resuelva la condicion de carrera ... clausula private 
+  int id = -1;
+  // 1. Genere una recion en paralelo con variable compartida id
+  // 3. Resuelva la condicion de carrera ... clausula private
 
-#ifdef _OPENMP
-omp_set_num_threads(4);
-#endif
+  #ifdef _OPENMP
+    omp_set_num_threads(4);
+  #endif
 
-#pragma omp parallel private(id)    
-{   
-//int id;
-// id es el nombre del hilo (hay condicion de carrera?)
-#ifdef _OPENMP
-id=omp_get_thread_num();
-#endif
-// 2. fuerce la condicion de carrera agregando sleep()
-sleep(1);
-printf("Peekaboo desde el hilo = %d \n",id);
-}  
+  #pragma omp parallel private(id)
+  {
+    // int id;
+    //  id es el nombre del hilo (hay condicion de carrera?)
+    #ifdef _OPENMP
+      id = omp_get_thread_num();
+    #endif
+
+    // 2. fuerce la condicion de carrera agregando sleep()
+    sleep(1);
+    printf("Peekaboo desde el hilo = %d \n", id);
+  }
 }
-
- 
-
--- Parallel/W8/Ejemp02_omp_for.cpp
+++ Parallel/W8/Ejemp02_omp_for.cpp
@@ -1,46 +1,50 @@
 #include <iostream>
 #ifdef _OPENMP
-#include<omp.h>
+  #include <omp.h>
 #endif
 #define CHUNKSIZE 3
-#define N 1<<4
+#define N 1 << 4
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-int i, id=0;
-float a[N], b[N], c[N];
+int main(int argc, char *argv[]) {
+  int i, id = 0;
+  float a[N], b[N], c[N];
 
-for (i=0; i < N; i++){
-  	a[i] = 2.0*i;
-	b[i] = i * 1.0;
-}
-
- printf("hilo /contador: \n");
-#pragma omp parallel private(id)
-{
-// particione N equitativamente entre hilos (puede usar nowait?)
-#pragma omp for schedule(static) private(i)
-// particione N equitativamente entre hilos pero con un CHUNKSIZE
-//#pragma omp for schedule(static,CHUNKSIZE) private(i) nowait
-// particione N en forma dinamica
-//#pragma omp for schedule(dynamic) private(i)
-// particione N en forma dinamica pero con un CHUNKSIZE
-//#pragma omp for schedule(dynamic,CHUNKSIZE) private(i)
-for (i=0; i < N; i++){
-#ifdef _OPENMP
-id=omp_get_thread_num();
-#endif
-	c[i] = a[i] + b[i];
- 	printf(" %i \t %i\n",id,i);
+  for (i = 0; i < N; i++) {
+    a[i] = 2.0 * i;
+    b[i] = i * 1.0;
   }
-//#pragma omp for 
-//for (i=0; i < N; i++) c[i]*=2; 
-}  /* end of parallel section */
 
+  printf("hilo /contador: \n");
 
-printf("Suma de vectores: \n");
-for (i=0; i < N; i++)
-	cout<<a[i]<<"\t"<<b[i]<<"\t"<<c[i]<<endl;
+  #pragma omp parallel private(id)
+  {
+    // particione N equitativamente entre hilos (puede usar nowait?)
+    // #pragma omp for schedule(static) private(i)
 
+    // particione N equitativamente entre hilos pero con un CHUNKSIZE
+    #pragma omp for schedule(static, CHUNKSIZE) private(i) nowait
+
+    // particione N en forma dinamica
+    // #pragma omp for schedule(dynamic) private(i)
+
+    // particione N en forma dinamica pero con un CHUNKSIZE
+    // #pragma omp for schedule(dynamic,CHUNKSIZE) private(i)
+    for (i = 0; i < N; i++) {
+      #ifdef _OPENMP
+        id = omp_get_thread_num();
+      #endif
+
+      c[i] = a[i] + b[i];
+      printf(" %i \t %i\n", id, i);
+    }
+
+    // #pragma omp for
+    // for (i=0; i < N; i++) c[i]*=2;
+  } /* end of parallel section */
+
+  printf("Suma de vectores: \n");
+
+  for (i = 0; i < N; i++)
+    cout << a[i] << "\t" << b[i] << "\t" << c[i] << endl;
 }
