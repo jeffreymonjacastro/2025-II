747e0932217696721726dd6411467c6b2347d978
Jeffrey Monja Castro <jeffrey.monja@utec.edu.pe>
2025-10-08 18:03:27 -0500

    Parallel w6

---
+++ Parallel/W6/ejemplo01a_bloqueada.cpp
@@ -0,0 +1,47 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv) {
+  int numtasks, rank, next, prev, tag1=1, tag2=2, N=1<<5;
+  int sbuf[N], rbuf[N];
+  double t0,t1;
+  MPI_Status status;
+  MPI_Init(&argc, &argv);                   // Initialize MPI
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Rank of the processor
+  MPI_Comm_size(MPI_COMM_WORLD, &numtasks); // Total number of processors
+
+prev=rank-1;
+next=rank+1;
+if(rank==0) prev=numtasks-1;
+if(rank==(numtasks-1)) next=0;
+
+t0=MPI_Wtime();
+
+// COMUNICACION CICLICA
+// 1. Cada proceso envía el buffer a su vecino dereco
+// y recibe de su vecino izquierdo
+//MPI_Send(&sbuf,N,MPI_INT,next,tag1,MPI_COMM_WORLD);
+//MPI_Recv(&rbuf,N,MPI_INT,prev,tag1,MPI_COMM_WORLD,&status);
+
+// 2. Utilice send sincrónico, ¿Qué sucede?
+//MPI_Ssend(&sbuf,N,MPI_INT,next,tag1,MPI_COMM_WORLD);
+//MPI_Recv(&rbuf,N,MPI_INT,prev,tag1,MPI_COMM_WORLD,&status);
+
+// NO-CICLICA
+// 3. Todos los procesos envian menos el ultimo. 
+// Todos los procesos reciben, menos el maestro
+
+if(rank<numtasks-1)
+	MPI_Send(&sbuf,N,MPI_INT,next,tag1,MPI_COMM_WORLD);
+if(rank>0)
+	MPI_Recv(&rbuf,N,MPI_INT,prev,tag1,MPI_COMM_WORLD,&status);
+
+t1=MPI_Wtime();
+
+if (rank==0) cout<<"Tiempo (s): "<<t1-t0<<endl;
+
+MPI_Finalize();
+
+}
---
+++ Parallel/W6/ejemplo01d_nobloqueada.cpp
@@ -0,0 +1,37 @@
+#include <mpi.h>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv) {
+  int numtasks, rank, next, prev,  tag1=1, tag2=2, N=1;//<<1;
+  int sbuf[N], rbuf[N];
+  double t0,t1;
+  MPI_Status stats[2];
+  MPI_Init(&argc, &argv);                   // Initialize MPI
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Rank of the processor
+  MPI_Comm_size(MPI_COMM_WORLD, &numtasks); // Total number of processors
+  MPI_Request reqs[4];
+
+prev=rank-1;
+next=rank+1;
+if(rank==0) prev=numtasks-1;
+if(rank==(numtasks-1)) next=0;
+
+sbuf[0]=10;
+
+t0=MPI_Wtime();
+//for(int i=0;i<100000;i++){
+MPI_Irecv(rbuf,N,MPI_INT,prev,tag1,MPI_COMM_WORLD,&reqs[0]);
+MPI_Isend(sbuf,N,MPI_INT,next,tag1,MPI_COMM_WORLD, &reqs[1]);
+for(int i=0;i<100000;i++) rbuf[0]++;
+printf("1.rank %d tiene el dato %d\n",rank,rbuf[0]);
+MPI_Waitall(2,reqs,stats);
+printf("2.rank %d tiene el dato %d\n",rank,rbuf[0]);
+//}
+t1=MPI_Wtime();
+
+if (rank==0) printf("Tiempo (s): %f \n",t1-t0);
+
+MPI_Finalize();
+
+}
